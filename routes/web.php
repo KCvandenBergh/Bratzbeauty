ad982be97f06b0972c1ff650284414ffef652196
KCvandenBergh <89838896+KCvandenBergh@users.noreply.github.com>
2023-10-11 11:50:49 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\TreatmentsController;
+use App\Http\Controllers\ColorsController;
+use App\Http\Controllers\DesignsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,24 +16,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about-us', function () {
-    return view('about-us');
-})->name('about-us');
-
-//Log in routes
-
-
-
-//Admin routes
+// wtf
+//Routes
+Route::get('/', function () {return view('welcome');});
+Route::get('/about-us', function () {return view('about-us'); })->name('about-us');
 
 
 //Resource routes
 Route::resource('treatments', TreatmentsController::class);
 Route::resource('reservations', ReservationController::class);
+Route::resource('colors', ColorsController::class);
+Route::resource('designs', DesignsController::class);
 
 
